6a91bbf5f22c8ef1efc0385fabaa5796126b9efc
Bestporter <32806123+Bestporter@users.noreply.github.com>
2022-04-23 12:36:32 +0800

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
-from app import index
+from src.app import index
+# from flask import Flask
+# import pytest
 
-def index_test():
+# @pytest.fixture(scope="session")
+def test_index():
+    # app = Flask(__name__)
     assert index() == "hello, world"
